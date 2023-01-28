9ae39b83fcf949e16a57aa1e926f01f692e88aea
pe712 <pe.baviere@gmail.com>
2023-01-29 00:25:36 +0100

    parsing json

--- src/Map.java
+++ src/Map.java
@@ -1,15 +1,26 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 
 public class Map {
     Site[] sites;
 
     public Map(String path) throws FileNotFoundException {
-        File file = new File(path);
-        Scanner scanner = new Scanner(file);
-        scanner.close();
-
         
+        FileInputStream file = new FileInputStream(path);
+        JSONTokener tokener = new JSONTokener(file);
+        JSONObject input = new JSONObject(tokener);
+        JSONObject parameters = input.getJSONObject("parameters");
+        JSONObject buildingCosts = parameters.getJSONObject("buildingCosts");
+        Long p = buildingCosts.getLong("productionCenter");
+        System.out.println(p);
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Map map = new Map("data/KIRO-tiny.json");
     }
 }
